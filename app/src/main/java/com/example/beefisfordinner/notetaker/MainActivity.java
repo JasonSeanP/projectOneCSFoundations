403e0b986170475f59a444f57812f83527813844
JasonSeanP <jasonseanp@gmail.com>
2017-11-26 19:29:16 -0600

    Add files necessary for FAB animation Include FAB animation code

--- app/src/main/java/com/example/beefisfordinner/notetaker/MainActivity.java
+++ app/src/main/java/com/example/beefisfordinner/notetaker/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,7 +21,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 //Public MainActivity class
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean isFabOpen = false;
+    private FloatingActionButton fab1,fab2,fab3;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
     EditText EditText1; //Declaring our EditText variable
     @Override
     protected void onCreate(Bundle savedInstanceState) { //onCreate method for when the app is loaded
@@ -26,7 +32,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //New Code
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab3 = (FloatingActionButton)findViewById(R.id.fab3);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        fab3.setOnClickListener(this);
+        //New Code
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab); //Declaring our FAB (Save icon)
         fab.setOnClickListener(new View.OnClickListener() { //When button is clicked...
             @Override
@@ -34,9 +53,50 @@ public class MainActivity extends AppCompatActivity {
                 Save("Note1.txt");
             }
         });
-
+        */
         EditText1 = (EditText) findViewById(R.id.EditText1); //Set EditText1 variable to the one we called in the content_main
         EditText1.setText(Open("Note1.txt"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fab1:
+                animateFAB();
+                break;
+            case R.id.fab2:
+                Log.d("Raj", "Fab 2");
+                break;
+            case R.id.fab3:
+                Log.d("Raj", "Fab 3");
+                break;
+        }
+    }
+
+    public void animateFAB(){
+
+        if(isFabOpen){
+
+            fab1.startAnimation(rotate_backward);
+            fab2.startAnimation(fab_close);
+            fab3.startAnimation(fab_close);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+            isFabOpen = false;
+            Log.d("Raj", "close");
+
+        } else {
+
+            fab1.startAnimation(rotate_forward);
+            fab2.startAnimation(fab_open);
+            fab3.startAnimation(fab_open);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj","open");
+
+        }
     }
 
     //Open menu when the icon is selected
